d1755beac889bce558d6a2f094bc3a5485221e80
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-08-09 15:20:05 -0300

    feat:criando base do jogo pelo DOM

---
+++ script.js
@@ -0,0 +1,17 @@
+const gameContainer = document.getElementById('game-container')
+
+const createTemplate = () => {
+    for(let i = 0; i < 7; i++){
+        let column = document.createElement('div')
+        gameContainer.appendChild(column)
+
+        for(let j = 0; j < 6; j++){
+            let line = document.createElement('div')
+            line.dataset.columnLine = `${i}-${j}`
+            line.classList.add('line')
+            column.appendChild(line)
+        }
+    }
+}
+
+createTemplate()
