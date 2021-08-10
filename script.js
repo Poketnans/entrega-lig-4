844f1af8896247d45a9d3d34aa5a346b3c4b059e
Naftaly Junior <juniorinterlyte95@hotmail.com>
2021-08-10 05:28:47 -0300

    fix: finalizado

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const createTemplate = () => {
 
         for(let j = 0; j < 6; j++){
             let line = document.createElement('div')
-            line.dataset.columnLine = `${i}-${j}`
+            line.dataset.columnLine = `${j}-${i}`
             line.classList.add('line')
             column.appendChild(line)
         }
@@ -16,7 +16,7 @@ const createTemplate = () => {
 
 createTemplate()
 
-const firstCells = document.querySelectorAll(".line[data-column-line$='-0']");
+const firstCells = document.querySelectorAll(".line[data-column-line|='0']");
 const columnsArray = [...firstCells].map( cell => cell.parentElement );
 
 let lastColor = 'red';
